20aa85ef30c8481016130d37130ade51e4ba8d96
Jim <jamesgallagher@hotmail.com>
2019-06-16 16:04:30 -0600

    cleaned responses

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,41 +7,50 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Password01",
+    password: "",
     database: "bamazon_db"
 });
 
 // Connect to the database
 connection.connect(function (err) {
     if (err) throw err;
-    start();
 });
 
 function start() {
 
+    // Displaying all products from the database  (need to be prettier!)    
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        console.log(results);
 
+        console.log('----------------------------------------------------------------------------------------------------')
+        for (var i = 0; i < results.length; i++) {
+            console.log("ID: " + results[i].item_id + " | " +
+                "Product: " + results[i].product_name + " | " +
+                "Department: " + results[i].department_name + " | " +
+                "Price: " + results[i].price + " | " +
+                "QTY: " + results[i].stock_quantity);
+            console.log('--------------------------------------------------------------------------------------------------')
+        }
+
+        // Prompt user for item and quantity
         inquirer
             .prompt([
                 {
                     name: "item",
-                    message: "Select an item",
-                    type: "list",
-                    choices: function () {
-                        var productsArray = [];
-                        for (let i = 0; i < results.length; i++) {
-                            productsArray.push(results[i].product_name);
+                    type: "input",
+                    message: "Enter the ID of the item you would like to purchase:",
+                    validate: function (value) {
+                        if (isNaN(value) === false && (value < 11)) {    
+                            return true;
                         }
-                        return productsArray;
-                    }
+                        return false;
+                    } 
                 },
                 {
                     name: "quantity",
-                    message: "Enter the number of items to purchase",
                     type: "input",
-                    validate: function(value) {
+                    message: "Enter the number of items you would like to purchase:",
+                    validate: function (value) {
                         if (isNaN(value) === false) {
                             return true;
                         }
@@ -49,8 +58,33 @@ function start() {
                     }
                 }
             ])
-            .then
+            .then(function (answer) {
+                let itemSelected = answer.item - 1;
+                let productName = results[itemSelected].product_name;
+                let itemQuantity = answer.quantity;
+                let finalCost = results[itemSelected].price * itemQuantity;
 
+                // If stock is available, print cost of purchase
+                if (itemQuantity < results[itemSelected].stock_quantity) {
+                    console.log("Your total for " + itemQuantity + " x '" + productName + "' is: $" +
+                    finalCost.toFixed(2));
 
+                // Update the database based on user purchase
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [{stock_quantity: results[itemSelected].stock_quantity - itemQuantity },
+                        {item_id: results[itemSelected].item_id }],
+                        function (err, results) {
+                            if (err) throw err;
+                        });
+                    connection.end();
+                // If not enough stock, notify user
+                } else {
+                    console.log("Unfortunately, there is not enough stock.")
+                    connection.end();
+
+                }
+            })
     });
 }
+
+start();
